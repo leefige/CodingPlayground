c70b74b4ccf7aacf5ec3a6982af6b0c4547041f4
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-10-09 21:00:23 +0800

    Pause trying on blockly. Move to use textarea instead firstly to test game logic.

--- react-frontend/src/components/ProgramItem/BlocklyPad.js
+++ react-frontend/src/components/ProgramItem/BlocklyPad.js
@@ -1,96 +1,56 @@
 import React, { Component } from 'react'
 
 class BlocklyPad extends Component {
-    blockly_ac_comp() {
-        "../../../node_modules/blockly/blockly_accessible_compressed.js";
+
+  // following funcs can only be used in text version
+  
+  constructor() {
+    super()
+    this.state = {
+      code: ''
     }
+  }
 
-    blocks_comp() {
-        "../../../node_modules/blockly/blocks_compressed.js";
-    }
+  handleCodeChange(event) {
+    this.setState({
+      code: event.target.value
+    })
+  }
 
-    blockly_comp() {
-        "../../../node_modules/blockly/blockly_compressed.js";
-    }
+  // following funcs can also be used in blockly version
 
-    msg() {
-        "../../../node_modules/blockly/msg/js/zh-hans.js";
-    }
+  genCode() {
+    // generate code from blockly
+    // currently just use original code
+    return this.state.code;
+  }
 
-    analytics() {
-        "./analytics.js";
-    }
+  handleCodeSubmit() {
+    const codeGenerated = this.genCode(this.code)
+    this.props.onCodeSubmit(codeGenerated)//回调函数，由父类实现
+  }
 
-    func() {
-        var blocklyArea = document.getElementById('blocklyArea');
-        var blocklyDiv = document.getElementById('blocklyDiv');
-        var workspace = window.Blockly.inject(blocklyDiv,
-            {media: '../../media/',
-            toolbox: document.getElementById('toolbox')});
-        var onresize = function(e) {
-            // Compute the absolute coordinates and dimensions of blocklyArea.
-            var element = blocklyArea;
-            var x = 0;
-            var y = 0;
-            do {
-            x += element.offsetLeft;
-            y += element.offsetTop;
-            element = element.offsetParent;
-            } while (element);
-            // Position blocklyDiv over blocklyArea.
-            blocklyDiv.style.left = x + 'px';
-            blocklyDiv.style.top = y + 'px';
-            blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-            blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-        };
-        window.addEventListener('resize', onresize, false);
-        onresize();
-        Window.Blockly.svgResize(workspace);
-    }
+  render() {
+    return (
+      <div>
+        <div>输入代码</div>
+        <div>
+          <textarea 
+            value={this.state.code}
+            onChange={this.handleCodeChange.bind(this)} />
+        </div>
 
-    createMarkup() { 
-        return {__html: 
-            '<script src="../../../node_modules/blockly/blocks_compressed.js"></script><script src="../../../node_modules/blockly/blocks_compressed.js"></script><script src="../../../node_modules/blockly/blockly_compressed.js"></script><script src="../../../node_modules/blockly/msg/js/zh-hans.js"></script><div id="blocklyDiv" style="height: 480px; width: 600px;"></div><xml id="toolbox" style="display: none"><block type="controls_if"></block><block type="logic_compare"></block><block type="controls_repeat_ext"></block><block type="math_number"></block><block type="math_arithmetic"></block><block type="text"></block><block type="text_print"></block></xml><script>var demoWorkspace = Blockly.inject(\'blocklyDiv\',{media: \'..\/..\/media\/\',toolbox: document.getElementById(\'toolbox\')});</script>'
-        }; 
-    };
-    
+        <div className='text-right'>
+          <button type="submit" 
+            className="btn btn-outline-primary" 
+            onClick = {this.handleCodeSubmit.bind(this)}>
+            运行
+          </button>
+        </div>
+      </div>
 
-
-    render() {
-        return (
-            <div dangerouslySetInnerHTML={this.createMarkup()} />
-            // <div className='blockly_pad'>
-
-            //     
-        
-        
-            //     <table>
-            //     <tbody>
-            //     <tr>
-            //         <td id="blocklyArea">
-            //         </td>
-            //     </tr>
-            //     </tbody></table>
-            
-            //     <div id="blocklyDiv">
-            //         <div class="injectionDiv" dir="LTR"></div>
-            //     </div>
-
-            //     <xml id="toolbox">
-            //     <block type="controls_if"></block>
-            //     <block type="logic_compare"></block>
-            //     <block type="controls_repeat_ext"></block>
-            //     <block type="math_number"></block>
-            //     <block type="math_arithmetic"></block>
-            //     <block type="text"></block>
-            //     <block type="text_print"></block>
-            //     </xml>
-            
-            //     {this.func()}
-            //     <div class="blocklyWidgetDiv"></div><div class="blocklyTooltipDiv"></div>
-            // </div>
-        )
-    }
+    )
+  }
 }
 
 export default BlocklyPad
