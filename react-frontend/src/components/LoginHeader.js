b318536496fd7fefdb493f4fc77bbe3cf0b706ac
张卡尔 <zke15@mails.tsinghua.edu.cn>
2017-10-11 22:02:05 +0800

    add connection to database for login and signup, very good

--- react-frontend/src/components/LoginHeader.js
+++ react-frontend/src/components/LoginHeader.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class LogoutHeader extends Component {
 	static defaultProps = {
@@ -26,12 +28,58 @@ class SignupHeader extends Component {
       email: '',
       password: ''
     }
-  }
-	handleLogin() {
-		this.props.onLogin('Karl')
 	}
-	handleSignup() {
-		this.props.onSignup()
+	async handleLogin() {
+		fetch('http://127.0.0.1:7001/user/login', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				"id": this.state.email,
+				"password": this.state.password,
+			})
+		})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			if (responseJson.signup_success)
+				console.log("Login success!")
+			else
+				console.log("Login failed!")
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+		this.props.onLogin(this.state.email)
+	}
+
+	async handleSignup() {
+		console.log(this.state.email)
+		fetch('http://127.0.0.1:7001/user/signup', {
+			method: 'POST',
+			mode: 'no-cors',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				"id": this.state.email,
+				"password": this.state.password,
+			})
+		})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			if (responseJson.signup_success)
+				console.log("Sign up success!")
+			else
+				console.log("Sign up failed!")
+		})
+		.catch((error) => {
+			console.log("error");
+			console.error(error);
+		});
 	}
 
 	handleEmailChange(event) {
@@ -80,23 +128,29 @@ class SignupHeader extends Component {
 					</div>
 				</div>
 
-				<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#signupModal">
-					Launch demo modal
-				</button>
-
-				<div className="modal fade" id="signupModal" tabindex="-1" role="dialog">
-					<div className="modal-dialog" role="document">
-						<div className="modal-content">
-							<div className="modal-header">
-								<button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-								<h4 className="modal-title" id="myModalLabel">Modal title</h4>
-							</div>
+				<button type="button" className="btn btn-primary pull-right" data-toggle="modal" data-target="#signupModal">注册</button>
+				<div className="modal fade clearfix text-left" id="signupModal" tabindex="-1" role="dialog">
+					<div className="modal-dialog modal-sm" role="document">
+						<div className="modal-content text-left">
 							<div className="modal-body">
-								...
-							</div>
-							<div className="modal-footer">
-								<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-								<button type="button" className="btn btn-primary">Save changes</button>
+								<div className="container">
+									<div className="row">
+										<div className="col-md-10">
+								<h4>请注册</h4>
+								</div>
+								<div className="col-md-1 col-md-offset-11">
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+										</div>
+										</div>
+									<form className="form-signin">
+										<label for="inputEmail" className="sr-only">邮箱</label>
+										<input type="email" id="inputEmail" className="form-control" placeholder="邮箱" required autofocus value = {this.state.email}  onChange={this.handleEmailChange.bind(this)}/>
+										<label for="inputPassword" className="sr-only">密码</label>
+										<input type="password" id="inputPassword" className="form-control" placeholder="密码" required value={this.state.password} onChange={this.handlePasswordChange.bind(this)}/>
+										<button className="btn btn-lg btn-success btn-block" type="submit" data-dismiss="modal" onClick={this.handleSignup.bind(this)}>注册</button>
+									</form>
+
+								</div>
 							</div>
 						</div>
 					</div>
