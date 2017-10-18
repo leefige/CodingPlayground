1b853804bcd976aec4121db91a6516b60ff59179
mym <845285227@qq.com>
2017-10-18 16:50:26 +0800

    30-unit-test, add test unit

--- docker-backend/egg-backend/test/controller/user.test.js
+++ docker-backend/egg-backend/test/controller/user.test.js
@@ -2,7 +2,7 @@
 const { app, mock, assert } = require('egg-mock/bootstrap');
 describe('test/controller/user.test.js', () => {
   afterEach(mock.restore);
-  describe('POST /', () => {
+  describe('signup test', () => {
     it('should status 200 and get the body', () => {
       // 对 app 发起 `POST /` 请求
       return app.httpRequest()
@@ -47,6 +47,11 @@ describe('test/controller/user.test.js', () => {
       // 使用 generator function 方式写测试用例，可以在一个用例中串行发起多次请求
       await app.httpRequest()
         .post('/user/signup')
+        .type('json')
+        .send({
+          id: '2341',
+          password: '213',
+        })
         .expect(200) // 期望返回 status 200
         .expect({
           signup_success: true,
@@ -54,6 +59,11 @@ describe('test/controller/user.test.js', () => {
       // 再请求一次
       const result = awiat app.httpRequest()
         .get('/user/signup')
+        .type('json')
+        .send({
+          id: '2341',
+          password: '213',
+        })
         .expect(200)
         .expect({
           signup_success: true,
@@ -62,4 +72,76 @@ describe('test/controller/user.test.js', () => {
       assert(result.status === 200);
     });*/
   });
+
+  describe('login test', () => {
+    it('should status 200 and get the body', () => {
+      // 对 app 发起 `POST /` 请求
+      return app.httpRequest()
+        .post('/user/login')
+        .type('json')
+        .send({
+          id: '2341',
+          password: '213',
+        })
+        .expect(200) // 期望返回 status 200
+        .expect({
+            login_success: true,
+        });
+    });
+
+    it('should get null when user not exists', async function () {
+      const ctx = app.mockContext();
+      const user = await ctx.service.user.login({
+        id: 'fengmk1',
+        password: '123',
+      });
+      assert(user);
+      assert(user.login_success === false);
+    });
+
+    it('should mock fengmk1 exists', async function () {
+      const ctx = app.mockContext();
+      app.mockService('user', 'login', async function () {
+        return {
+          signup_success: false,
+        };
+      });
+      const user = await ctx.service.user.login({
+        id: 'fengmk1',
+        password: '123',
+      });
+      assert(user);
+      assert(user.login_success === false);
+    });
+
+    /*it('should send multi requests', async function () {
+      // 使用 generator function 方式写测试用例，可以在一个用例中串行发起多次请求
+      await app.httpRequest()
+        .post('/user/login')
+        .type('json')
+        .send({
+          id: '2341',
+          password: '213',
+        })
+        .expect(200) // 期望返回 status 200
+        .expect({
+          login_success: true,
+      }); // 期望 body 是 hello world
+      // 再请求一次
+      const result = awiat app.httpRequest()
+        .get('/user/login')
+        .type('json')
+        .send({
+          id: '2341',
+          password: '213',
+        })
+        .expect(200)
+        .expect({
+          login_success: true,
+      });
+      // 也可以这样验证
+      assert(result.status === 200);
+    });*/
+  });
+
 });
--- docker-backend/egg-backend/test/service/user.test.js
+++ docker-backend/egg-backend/test/service/user.test.js
@@ -1,20 +1,52 @@
 'use strict';
-describe('signup()', () => {
+const { app, mock, assert } = require('egg-mock/bootstrap');
+describe('test/service/user.test.js', () => {
+  afterEach(mock.restore);
+  describe('signup()', () => {
     // 因为需要异步调用，所以使用 generator function
     it('should signup user', async function () {
       // 创建 ctx
       const ctx = app.mockContext();
       // 通过 ctx 访问到 service.user
-      const result = await ctx.service.user.signup({
+      const user = await ctx.service.user.signup({
           id: 'asd',
           password: 'cxv',
       });
       assert(user);
       assert(user.signup_success === true);
     });
-    it('should get null when user not exists', function* () {
+    it('should get false when user exists', function* () {
       const ctx = app.mockContext();
-      const user = await ctx.service.user.get('fengmk1');
-      assert(!user);
+      const user = await ctx.service.user.login({
+        id: 'fengmk1',
+        password: '123',
+      });
+      assert(user);
+      assert(user.signup_success === false);
     });
   });
+
+  describe('login()', () => {
+    // 因为需要异步调用，所以使用 generator function
+    it('should login user', async function () {
+      // 创建 ctx
+      const ctx = app.mockContext();
+      // 通过 ctx 访问到 service.user
+      const user = await ctx.service.user.login({
+          id: 'asd',
+          password: 'cxv',
+      });
+      assert(user);
+      assert(user.signup_success === true);
+    });
+    it('should get false when id or password error', function* () {
+      const ctx = app.mockContext();
+      const user = await ctx.service.user.login({
+        id: 'fengmk1',
+        password: '123',
+      });
+      assert(user);
+      assert(user.login_success === false);
+    });
+  });
+});
