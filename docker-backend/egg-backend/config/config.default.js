c28a953dff14e8c0387bf2505194acc77481852d
毛亦铭 <maoym15@mails.tsinghua.edu.cn>
2017-10-20 23:40:58 +0800

    #52, fix cookies and cors bug

--- docker-backend/egg-backend/config/config.default.js
+++ docker-backend/egg-backend/config/config.default.js
@@ -16,18 +16,24 @@ module.exports = appInfo => {
     },
     security: {
       csrf: false,
-      domainWhiteList: [ 'http://localhost:3000' ],
+      //domainWhiteList: [ 'http://localhost:3000' ],
       // {
       // queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
       // bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
     // },
     },
-
+    session: {
+      key: 'EGG_SESS',
+      maxAge: 24 * 3600 * 1000, // 1 天
+      httpOnly: true,
+      encrypt: true,
+    }
   };
-  // config.cors = {
-    // origin:'*',
-    // allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
-    // }
+  config.cors = {
+    origin:'http://localhost:3000',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+    credentials: true,
+    }
   // should change to your own
   config.keys = appInfo.name + '_egg';
 
