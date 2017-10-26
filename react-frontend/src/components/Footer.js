62bef43a96561ced5f8bda59d848c9c3d4ac571f
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-10-26 11:24:21 +0800

    #70 Add navigation button in footer layout.

--- react-frontend/src/components/Footer.js
+++ react-frontend/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className='row'>
-                <span>Footer</span>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='row'>
+        <nav>
+          <span><Link to='251' className="btn btn-outline-primary">Stage 1</Link></span>
+          <span><Link to='252' className="btn btn-outline-primary">Stage 2</Link></span>
+          <span><Link to='253' className="btn btn-outline-primary">Stage 3</Link></span>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Footer
