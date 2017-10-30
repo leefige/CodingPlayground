936a96b3cb8ffd7bc153f2790ae418fc781c8325
毛亦铭 <maoym15@mails.tsinghua.edu.cn>
2017-10-30 19:13:28 +0800

    #82 fix local file map bug

--- docker-backend/egg-backend/app/service/map.js
+++ docker-backend/egg-backend/app/service/map.js
@@ -26,12 +26,21 @@ module.exports = app => {
         "primary key (id)" +
         ");";
         await app.mysql.query(sql);*/
-        const _data1 = {};
+        const result = await this.ctx.curl('localhost:7001/public/test.json', {
+          // 自动解析 JSON response
+          dataType: 'json',
+          // 3 秒超时
+          timeout: 3000,
+        });
+        
+        const _data1 = result.data;
         const data1 = JSON.stringify(_data1);
-        const is_insert1 = await app.mysql.get('map', { id: 250 });
+        const is_insert1 = await app.mysql.get('map', { id: 274 });
         if(is_insert1 === null)
-          await app.mysql.insert('map', { id: 260, data: data1 });
-        return true;
+          await app.mysql.insert('map', { id: 274, data: data1 });
+        else
+          await app.mysql.update('map', { id: 274, data: data1 });
+        return _data1;
       } catch (err) {
         console.error(err);
         return false;
