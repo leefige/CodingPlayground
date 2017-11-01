52b430482514e783f0bde91acc7fac6eda1112b4
张卡尔 <zke15@mails.tsinghua.edu.cn>
2017-11-01 22:05:20 +0800

    #91 Move style to css file.

--- react-frontend/src/components/ProgramItem/BlocklyPad.js
+++ react-frontend/src/components/ProgramItem/BlocklyPad.js
@@ -36,29 +36,20 @@ class BlocklyPad extends Component {
           blocklyConfig={this.props.blocklyConfig}
           onXmlChange={this.props.onXmlChange.bind(this)} />
 
-        <div className='text-right' style={{ margin: 15 }}>
+        <div className='text-right blockly-btn-group'>
           <button type="button"
-            className="btn btn-danger"
-            style={{ marginRight: 15 }}
+            className="btn btn-danger blockly-btn"
             onClick={this.props.onReset}>
             <span className="glyphicon glyphicon-stop"></span>
             &nbsp;终止
             </button>
           <button type="button"
-            className="btn btn-info"
-            style={{ marginRight: 15 }}>
-            <span className="glyphicon glyphicon-pause"></span>
-            &nbsp;暂停
-            </button>
-          <button type="button"
-            className="btn btn-warning"
-            style={{ marginRight: 15 }}>
+            className="btn btn-warning blockly-btn">
             <span className="glyphicon glyphicon-step-forward"></span>
             &nbsp;单步调试
             </button>
           <button type="button"
-            className="btn btn-success"
-            style={{ marginRight: 15 }}
+            className="btn btn-success blockly-btn"
             onClick={this.handleCodeSubmit.bind(this)}>
             <span className="glyphicon glyphicon-play"></span>
             &nbsp;生成并运行
