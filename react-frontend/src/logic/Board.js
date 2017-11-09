8277f274401742b5bebeda53bf00da510ad39da2
张卡尔 <zke15@mails.tsinghua.edu.cn>
2017-11-09 11:27:16 +0800

    #127 add chestPos interface.

--- react-frontend/src/logic/Board.js
+++ react-frontend/src/logic/Board.js
@@ -1,7 +1,7 @@
 import Basic from "./Basic"
 const elements = {
   empty : 0,
-  treasure : 1,
+  chest : 1,
   barrier : 2,
   grass : 4,
   stone : 5,
@@ -13,7 +13,17 @@ class Board extends Basic {
     super(state);
     this._size = this._state.map.length;
     this._elements = elements;
+    this._chestPos = { x : -1, y : -1};
+    // get treasurePos
+    for (let i = 0; i < this._size; i++) {
+      for (let j = 0; j < this._size; j++) {
+        if (this.map[i][j] === elements.chest) {
+          this._chestPos = { x:i, y:j};
+        }
+      }
+    }
   }
+  get chestPos() { return this._chestPos; }
   get map() { return this._state.map; }
   get size() { return this._size; }
   get elements() { return this._elements; }
