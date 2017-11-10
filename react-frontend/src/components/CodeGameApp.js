941ff6f116bf412314600726bb14a001f4c92623
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-11-10 09:57:16 +0800

    #130 Add basic personal page layout; refine .css intend.

--- react-frontend/src/components/CodeGameApp.js
+++ react-frontend/src/components/CodeGameApp.js
@@ -1,10 +1,11 @@
-import CodeGameContent from '../pages/Gaming/CodeGameContent'
-import MapEditor from '../pages/MapEditor/MapEditor'
-import React, { Component } from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import Signup from './Signup'
-import Login from './Login'
+import CodeGameContent from '../pages/Gaming/CodeGameContent';
+import MapEditor from '../pages/MapEditor/MapEditor';
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Signup from './Signup';
+import Login from './Login';
+import Personal from '../pages/Personal/Personal';
 import {
   BrowserRouter as Router,
   Route,
@@ -46,19 +47,18 @@ class CodeGameApp extends Component {
       <div>
         <div className="container-fluid">
           <div className='game-header row'>
-          <div className="col-md-offset-4 btn-group clear-fix text-left pull-left" role="group" aria-label="...">
-          <Link to='/map/301' type="button" class="btn btn-default">1</Link>
-          <Link to='/map/302' type="button" class="btn btn-default">2</Link>
-          <Link to='/map/303' type="button" class="btn btn-default">3</Link>
-          <Link to='/map/304' type="button" class="btn btn-default">4</Link>
-          <Link to='/map/305' type="button" class="btn btn-default">5</Link>
-          <Link to='/map/306' type="button" class="btn btn-default">6</Link>
-          <Link to='/map/307' type="button" class="btn btn-default">7</Link>
-          <Link to='/map/308' type="button" class="btn btn-default">8</Link>
-          <Link to='/map/309' type="button" class="btn btn-default">9</Link>
-          <Link to='/map/310' type="button" class="btn btn-default">10</Link>
-          
-        </div>
+            <div className="col-md-offset-4 btn-group clear-fix text-left pull-left" role="group" aria-label="...">
+              <Link to='/map/301' type="button" class="btn btn-default">1</Link>
+              <Link to='/map/302' type="button" class="btn btn-default">2</Link>
+              <Link to='/map/303' type="button" class="btn btn-default">3</Link>
+              <Link to='/map/304' type="button" class="btn btn-default">4</Link>
+              <Link to='/map/305' type="button" class="btn btn-default">5</Link>
+              <Link to='/map/306' type="button" class="btn btn-default">6</Link>
+              <Link to='/map/307' type="button" class="btn btn-default">7</Link>
+              <Link to='/map/308' type="button" class="btn btn-default">8</Link>
+              <Link to='/map/309' type="button" class="btn btn-default">9</Link>
+              <Link to='/map/310' type="button" class="btn btn-default">10</Link>
+            </div>
         
             <div className='pull-right'>
               <Header
@@ -79,10 +79,11 @@ class CodeGameApp extends Component {
           <Route path="/mapEditor" component={MapEditor} />
           <Route path="/login" component={props => <Login {...props} onLogin={this.handleLogin.bind(this)} />} />
           <Route path="/signup" component={Signup} />
+          <Route path="/personal" component={props => <Personal {...props} userId={this.getLoginUserId.bind(this)}/>} />
           <Footer className='footer-style' />
         </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ react-frontend/src/pages/Personal/Personal.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { post } from "../../utils/Request"
+
+
+class Personal extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      rememberMe: false,
+    }
+
+  }
+
+  async handleSignup() {
+    post('/user/signup', {
+      id: this.state.email,
+      password: this.state.password,
+    })
+      .then((responseJson) => {
+        if (responseJson.signup_success)
+          alert("注册成功！");
+        else
+          alert("注册失败！");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  handleEmailChange(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handlePasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className='col-md-8 col-md-offset-2'>
+        <div className="form-box">
+          <div className="form-top">
+            <div className="form-top-left">
+              <h3>欢迎来到CodingPlayground！</h3>
+              <h4>赶快注册吧：</h4>
+            </div>
+          </div>
+          <div className="form-bottom">
+            <form role="form" className="login-form">
+              <div className="form-group">
+                <label className="sr-only" for="form-email">Email</label>
+                <input
+                  type="text"
+                  name="form-email"
+                  placeholder="邮箱"
+                  className="form-email form-control"
+                  id="form-email"
+                  required
+                  autofocus
+                  value={this.state.email}
+                  onChange={this.handleEmailChange.bind(this)}
+                  />
+              </div>
+              <div className="form-group">
+                <label className="sr-only" for="form-password">Password</label>
+                <input
+                  type="password"
+                  name="form-password"
+                  placeholder="密码"
+                  className="form-password form-control" id="form-password"
+                  required
+                  value={this.state.password}
+                  onChange={this.handlePasswordChange.bind(this)}
+                  />
+              </div>
+              <button type="button" className="btn btn-success btn-form" onClick={this.handleSignup.bind(this)}>注册！</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Personal;
