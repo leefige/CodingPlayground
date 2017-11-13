5838a1c7e564732f9a2c0002745b79dcdd138ea1
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-11-13 22:34:45 +0800

    #130 Redefine action of form for account setting.

--- react-frontend/src/pages/Personal/Account.js
+++ react-frontend/src/pages/Personal/Account.js
@@ -1,35 +1,53 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { post } from "../../utils/Request"
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { post } from "../../utils/Request";
 
 
 class Account extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       id: 'user ID',
       email: '',
       oldPassword: '',
       newPassword: '',
       againPassword: '',
-    }
-
+    };
   }
 
-  async handleSignup() {
-    post('/user/signup', {
-      id: this.state.email,
-      password: this.state.password,
-    })
-      .then((responseJson) => {
-        if (responseJson.signup_success)
-          alert("注册成功！");
+  async handleSubmitEmail(event) {
+    alert("submit email");
+    post('/user/change/email', {
+      email: this.state.email,
+    }).then((responseJson) => {
+        if (responseJson.change_email_success)
+          alert("修改成功！");
         else
-          alert("注册失败！");
-      })
-      .catch((error) => {
+          alert("修改失败！");
+    }).catch((error) => {
+      console.error(error);
+    });
+    event.preventDefault();
+  }
+
+  async handleSubmitPassword(event) {
+    if (this.state.newPassword !== this.state.againPassword) {
+      alert("两次输入的密码不同！");
+    } else {
+      alert("submit psw");
+      post('/user/change/password', {
+        oldPassword: this.state.oldPassword,
+        newPassword: this.state.newPassword,
+      }).then((responseJson) => {
+          if (responseJson.change_email_success)
+            alert("修改成功！");
+          else
+            alert("修改失败！");
+      }).catch((error) => {
         console.error(error);
       });
+    }
+    event.preventDefault();
   }
 
   handleEmailChange(event) {
@@ -64,10 +82,6 @@ class Account extends Component {
 
   }
 
-  getUserID() {
-    return "user ID";
-  }
-
   render() {
     return (
       <div className='col-md-8 col-md-offset-2'>
@@ -77,15 +91,12 @@ class Account extends Component {
             <h5>Account Management</h5>
           </div>
           <hr/>
-          <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" action="/updateProfile" accept-charset="UTF-8" method="post">
+          {/* <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" action="/updateProfile" accept-charset="UTF-8" method="post"> */}
             <div className="row">
               <div className="col-lg-3">
                 <h4>我的头像</h4>
-                <div className="clearfix avatar-image append-bottom-default">
-                  <img alt="" className="avatar s160" src="http://47.94.142.165:8088/gitlab/uploads/user/avatar/6/avatar.png" />
-                </div>
               </div>
-              <div className="col-lg-9">
+              <div className="col-lg-4">
                 <h5 className="prepend-top-0">更换头像</h5>
                 <div className="prepend-top-5 append-bottom-10">
                   <div>
@@ -102,10 +113,15 @@ class Account extends Component {
                 </div>
                 <div className="help-block">图片最大尺寸为200KB</div>
               </div>
+              <div className="col-lg-4 col-lg-offset-1">
+                <div className="clearfix avatar-image append-bottom-default">
+                  <img alt="" className="avatar s160" src="http://47.94.142.165:8088/gitlab/uploads/user/avatar/6/avatar.png" />
+                </div>
+              </div>
             </div>
-          </form>
+          {/* </form> */}
           <hr/>
-          <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" action="/updateProfile" accept-charset="UTF-8" method="post">
+          <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" onSubmit={this.handleSubmitEmail.bind(this)} accept-charset="UTF-8" method="post">
             <div className="row">
               <div className="col-lg-3 profile-settings-sidebar">
                 <h4>账号设置</h4>
@@ -119,18 +135,18 @@ class Account extends Component {
                 <div className="form-group">
                   <label className="label-light" for="user_email">绑定邮箱</label>
                   <input className="personal-control" required="required" 
-                    type="text" value={this.state.email} onChange={this.handleEmailChange.bind(this)}
+                    type="email" value={this.state.email} onChange={this.handleEmailChange.bind(this)}
                     placeholder="sample@xyz.com" name="user[email]" id="user_email" />
                 </div>
                 <div className="prepend-top-default append-bottom-default">
-                  <input type="submit" name="commit" value="确认提交" className="btn btn-success" />
+                  <input type="submit" name="commit" value="确认提交"  className="btn btn-success" />
                   <a className="btn btn-cancel" href="/personal">取消修改</a>
                 </div>
               </div>
             </div>
           </form>
           <hr/>
-          <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" action="/updateProfile" accept-charset="UTF-8" method="post">
+          <form className="edit-user prepend-top-default" id="edit_user_6" enctype="multipart/form-data" onSubmit={this.handleSubmitPassword.bind(this)} accept-charset="UTF-8" method="post">
             <div className="row">
               <div className="col-lg-3">
                 <h4>修改密码</h4>
