d9076aecb3d5e01f3713bb65e41e431e8f44a0f5
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-11-15 01:32:49 +0800

    #141 Add new mini map; add hover style.

--- react-frontend/src/pages/Level/Level.js
+++ react-frontend/src/pages/Level/Level.js
@@ -20,17 +20,24 @@ class Level extends Component {
     if (this.state.dest === 0) {
       return (
         <div className='col-md-8 col-md-offset-2'>
-          <MapCard levelNumber="1" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="2" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="3" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="4" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="5" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="6" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="7" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="8" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="9" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="10" onSelectLevel={this.handleLevelClick.bind(this)}/>
-          <MapCard levelNumber="-1" onSelectLevel={this.handleLevelClick.bind(this)}/>
+          <div className='row'>
+            <MapCard levelNumber="1" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="2" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="3" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="4" onSelectLevel={this.handleLevelClick.bind(this)}/>
+          </div>
+          <div className='row'>
+            <MapCard levelNumber="5" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="6" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="7" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="8" onSelectLevel={this.handleLevelClick.bind(this)}/>
+          </div>
+          <div className='row'>
+            <MapCard levelNumber="9" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="10" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="11" onSelectLevel={this.handleLevelClick.bind(this)}/>
+            <MapCard levelNumber="-1" onSelectLevel={this.handleLevelClick.bind(this)}/>
+          </div>
         </div>
       );
     }
--- react-frontend/src/pages/Level/MapCard.js
+++ react-frontend/src/pages/Level/MapCard.js
@@ -16,7 +16,12 @@ class MapCard extends Component {
   }
 
   handleClick() {
-    this.props.onSelectLevel(this.state.level);
+    if (this.state.level <= 10) {
+      this.props.onSelectLevel(this.state.level);
+    }
+    else {
+      return;
+    }
   }
 
   render() {
@@ -25,19 +30,23 @@ class MapCard extends Component {
         <div className="card-content">
           <div className="row">
             <div className="col-lg-12">
-              {this.state.level > 0 && this.state.level < 5 ?
-                <img className="minimap" src={"/img/mini_map/" + this.props.levelNumber + ".png"} /> :
-                this.state.level < 0 ?
-                  <img className="minimap" src={"/img/mini_map/edit.png"} /> :
-                  <img className="minimap" src={"/img/mini_map/lock.jpg"} />
+              {this.state.level > 0 ? (
+                this.state.level <= 10 ?
+                    <img className="minimap" alt={"第" + this.props.levelNumber + "关"} src={"/img/mini_map/" + this.props.levelNumber + ".png"} /> :
+                    <img className="minimap" alt="敬请期待" src={"/img/mini_map/lock.jpg"} />
+                ) :
+                <img className="minimap" alt="地图编辑器" src={"/img/mini_map/edit.png"} />
               }
             </div>
           </div>
           <div className="row">
             <div className="col-lg-12">
               <div className="level-title">
-                {this.state.level > 0 ?
-                  <h4>- 第 <span> {this.props.levelNumber} </span> 关 -</h4> :
+                {this.state.level > 0 ? (
+                  this.state.level <= 10 ?
+                    <h4>- 第 <span> {this.props.levelNumber} </span> 关 -</h4> :
+                    <h4>- 敬请期待 -</h4>
+                  ) :
                   <h4>- 地图编辑器 -</h4>
                 }
               </div>
