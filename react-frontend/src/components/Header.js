111e89b71a601238c9a8b4be6a8a80a0c86d7b62
张卡尔 <zke15@mails.tsinghua.edu.cn>
2017-11-16 03:12:06 +0800

    #153 Fix bugs of logout, add redirect.

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import { post } from "../utils/Request";
 import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 class Header extends Component {
-  handleLogout() {
-    this.props.onLogout();
+  constructor() {
+    super();
+    this.state = {
+      didAutoLogin: false,
+      isAutoLogin: false,
+    }
+  }
+  async handleLogout() {
+    post('/user/logout', {})
+    .then((responseJson) => {
+      if (responseJson.logout_success) {
+        this.props.onLogout();
+        this.setState({
+          isAutoLogin : false,
+        })
+        console.log("logout success")
+      }
+      else
+        alert("登录失败！");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 
   componentWillMount() {
@@ -14,12 +36,24 @@ class Header extends Component {
   }
 
   async autoLogin() {
+    this.setState({
+      didAutoLogin : false,
+    })
     post('/user/autoLogin', {
       autoLogin: true,
     })
       .then((responseJson) => {
         if (responseJson.autoLogin_success) {
+          this.setState({
+            didAutoLogin : true,
+            isAutoLogin : true,
+          })
           this.props.onLogin(responseJson.id);
+        }
+        else {
+          this.setState({
+            didAutoLogin : true,
+          })
         }
       })
       .catch((error) => {
@@ -30,10 +64,10 @@ class Header extends Component {
   render() {
     return (
       <div>
-        {this.props.isLogin ?
+        {this.props.isLogin || this.state.isAutoLogin ?
           <div className="logout-header">
             <Link to='/personal/account' type="button" className="btn btn-success header-btn">{this.props.id}</Link>
-            <Link to='/login' type="button" class="btn btn-default" onClick={this.handleLogout.bind(this)}>登出</Link>
+            <button type="button" className="btn btn-default" onClick={this.handleLogout.bind(this)}>登出</button>
           </div>
           :
           <div className="login-header">
@@ -41,6 +75,12 @@ class Header extends Component {
             <Link to='/signup' type="button" className="btn btn-default header-btn">注册</Link>
           </div>
         }
+        {this.state.didAutoLogin === true && this.state.isAutoLogin === false && this.props.pathname !== '/login' && this.props.pathname !== '/signup'
+          ? <Redirect push to="/login"/>
+          : <div></div>}
+        {(this.state.isAutoLogin === true || this.props.isLogin === true) && (this.props.pathname === '/login' || this.props.pathname === '/signup')
+          ? <Redirect push to="/index"/>
+          : <div></div>}
       </div>
     )
   }
